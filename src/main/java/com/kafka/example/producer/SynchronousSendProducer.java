941048fa7055fae2de97d22bee7258199894c993
袁文界 <yuan.wenjie@trs.com.cn>
2017-11-11 11:14:23 +0800

    SynchronousSendProducer

--- src/main/java/com/kafka/example/producer/SynchronousSendProducer.java
+++ src/main/java/com/kafka/example/producer/SynchronousSendProducer.java
@@ -1,15 +1,36 @@
 package com.kafka.example.producer;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Created by yuanwenjie on 2017/11/11.
  */
 public class SynchronousSendProducer {
-   /* private  final static String topic = "log";
+    private static final String topic = "topic_fireAndForget";
+    private static final Integer partition = 0;
+    private static final Long timestamp = null;
+    private static final String key = "key";
+    private static final String value = "--value--";
 
     public static void main(String[] args) {
-        ProducerRecord<String,String> record = new ProducerRecord<String, String>(topic,partition,timestamp,key,value);
-    }*/
-    
+        Properties kafkaProps = new Properties();
+        kafkaProps.put("bootstrap.servers", "10.143.20.199:9092");
+        kafkaProps.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        kafkaProps.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        kafkaProps.put("acks","-1");
+        kafkaProps.put("retries","3");
+        KafkaProducer producer = new KafkaProducer(kafkaProps);
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>(topic,null, value);
+        try {
+            producer.send(record).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
 }
