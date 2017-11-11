5d0b6bcb07a4d0281fa7aed72dd0f24c4a9a4d71
ynyuanwenjie <ywj81029>
2017-11-11 11:10:25 +0800

    fire and forget

--- src/main/java/com/kafka/example/producer/FireAndForgetProducer.java
+++ src/main/java/com/kafka/example/producer/FireAndForgetProducer.java
@@ -10,20 +10,20 @@ import java.util.Properties;
  */
 public class FireAndForgetProducer {
     private static final String topic = "topic_fireAndForget";
-    private static final Integer partition = 2;
+    private static final Integer partition = 0;
     private static final Long timestamp = null;
     private static final String key = "key";
-    private static final String value = "value";
+    private static final String value = "--value--";
 
     public static void main(String[] args) {
         Properties kafkaProps = new Properties();
         kafkaProps.put("bootstrap.servers", "10.143.20.199:9092");
         kafkaProps.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         kafkaProps.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        kafkaProps.put("acks","1");
+        kafkaProps.put("acks","-1");
         kafkaProps.put("retries","3");
         KafkaProducer producer = new KafkaProducer(kafkaProps);
-        ProducerRecord<String, String> record = new ProducerRecord<String, String>(topic, partition, timestamp, key, value);
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>(topic,null, value);
         producer.send(record);
     }
 
