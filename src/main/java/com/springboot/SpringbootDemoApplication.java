e302292cac2b51f174925e9281487ad7879e4267
yuanwenjie <ynyuanwenjie@gmail.com>
2019-03-13 16:24:59 +0800

    //

--- src/main/java/com/springboot/SpringbootDemoApplication.java
+++ src/main/java/com/springboot/SpringbootDemoApplication.java
@@ -1,23 +1,17 @@
 package com.springboot;
-
-import com.springboot.model.Reservation;
-import com.springboot.service.ReservationRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 @ComponentScan("com.springboot")
 @EntityScan("com.springboot")
-public class SpringbootDemoApplication extends SpringBootServletInitializer {
+@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
+public class SpringbootDemoApplication {
 
 	/*@Bean
     CommandLineRunner runner (ReservationRepository rr) {
@@ -35,4 +29,6 @@ public class SpringbootDemoApplication extends SpringBootServletInitializer {
 
     }
 }
+
+
 
--- src/test/java/com/springboot/start/SpringbootDemoApplicationTests.java
+++ src/test/java/com/springboot/start/SpringbootDemoApplicationTests.java
@@ -1,104 +1,16 @@
 package com.springboot.start;
 
-import com.springboot.SpringbootDemoApplication;
-import com.springboot.model.Reservation;
-import com.springboot.util.JacksonUtil;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
-
-import java.nio.charset.Charset;
-
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = SpringbootDemoApplication.class)
-@WebAppConfiguration
-//@ContextConfiguration(classes = WebConfig.class)
-//@WebAppConfiguration("src/test/webapp")
-//@WebAppConfiguration("classpath:test-web-resources")
+@SpringBootTest
 public class SpringbootDemoApplicationTests {
-    private MediaType contentType = new MediaType("application",
-            "json", Charset.forName("UTF-8"));
-    private MockMvc mockMvc;
-    @Autowired
-    private WebApplicationContext webApplicationContext;
 
-    /**
-     * 测试加载上下文
-     */
     @Test
     public void contextLoads() {
     }
 
-    @Before
-    public void setup() {
-        System.out.println("-------------------begin------------------------------------------");
-        this.mockMvc = webAppContextSetup(webApplicationContext).build();
-    }
-
-    @Test
-    public void testMock() {
-        System.out.println(this.mockMvc);
-    }
-
-    @Test
-    public void testReadReservation() throws Exception {
-        String urlTemplate = "reservation/name/jlong";
-        mockMvc.perform(get(urlTemplate))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(contentType))
-                .andExpect(content().json("{\"id\":8,\"reservationName\":\"jlong\"}"));
-    }
-
-    @Test
-    public void testReadReservationJsonPath() throws Exception {
-        String urlTemplate = "reservation/name/jlong";
-        mockMvc.perform(get(urlTemplate))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(contentType))
-                .andExpect(jsonPath("$.id", is("8")))
-                .andExpect(jsonPath("$.reservationName", is("jlong")));
-    }
-
-    @Test
-    public void testReadReservationJsonPathSize() throws Exception {
-        String urlTemplate = "reservation/name/jlong";
-        mockMvc.perform(get(urlTemplate))
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(contentType))
-                .andExpect(jsonPath("$", hasSize(2)))
-                .andExpect(jsonPath("$[1].id", is("8")))
-                .andExpect(jsonPath("$[1].reservationName", is("jlong")));
-    }
-
-    @Test
-    public void testSaveReservation() throws Exception {
-        Reservation reservation = new Reservation(null,"TestSave111");
-        String reservationJson = JacksonUtil.Obj2Json(reservation);
-        this.mockMvc.perform(post("reservation/save")
-                .contentType(contentType)
-                .content(reservationJson))
-                .andExpect(status().isOk());
-    }
-
-    @After
-    public void tearDown() {
-        System.out.println("-------------------end-------------------------------------");
-    }
 }
